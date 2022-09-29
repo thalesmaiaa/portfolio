4f96fa16b0d7f32019b2ad8094a16dcdacf59ca7
ThalesMaia <thalesmaiaalexandre@gmail.com>
2022-09-28 21:12:05 -0300

    resize navbar and change introduction style

--- components/introduction/introduction.tsx
+++ components/introduction/introduction.tsx
@@ -1,22 +1,40 @@
-import { Box, Container, Typography } from "@mui/material"
-import { styles } from "./styles"
+import { Box, Typography } from "@mui/material";
 
+import { classes } from "../../utils";
+
+import styles from "./introduction.module.scss";
 
 export const Introduction: React.FC = () => {
-    return (
-        <>
+  return (
+    <>
+      <Box className={styles.skillsContainer}>
+        <Typography className={styles.name} variant="h2">
+          Frontend Developer & Software Developer
+        </Typography>
+        <Typography className={styles.presentation} variant="h3">
+          {"My name is Thales and this is a website about me,"}
+          {
+            "  show a few technologies that i've been working recently and list some projects that you might want to see."
+          }
+        </Typography>
+        <Box className={styles.imagesArea}>
+          <div className={classes(styles.images, styles.desktopImage)}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/256/2920/2920244.png"
+              className={classes(styles.image, styles.webImage)}
+            />
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/6062/6062646.png"
+              className={classes(styles.image, styles.laptopImage)}
+            />
 
-        <Box sx={styles.skillsContainer}>
-                <Typography sx={styles.name} variant="h2">
-                    Hello, my name is THALES.
-                </Typography>
-                <Typography sx={styles.presentation} variant="h5">
-                          
-                   {"This is a website that talks about me, show a few technologies that i've been working recently and list some projects that you might want to see."} 
-                </Typography>
-            </Box>
-
-            
-        </>
-    )
-}
+            <img
+              src="https://cdn-icons-png.flaticon.com/256/644/644667.png"
+              className={classes(styles.image, styles.codeImage)}
+            />
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,35 +1,29 @@
-import { Box, IconButton, Typography } from "@mui/material"
-import Link from "next/link"
-import CodeIcon from '@mui/icons-material/Code';
-import { styles } from "./styles"
+import { Box, IconButton, Typography } from "@mui/material";
+import Link from "next/link";
+import CodeIcon from "@mui/icons-material/Code";
+
+import styles from "./navbar.module.scss";
 
 export const Navbar: React.FC = () => {
-    return (
-        <>
-        
-            <Box 
-            sx={styles.navContainer}>
-                <IconButton sx={styles.codeIcon}>
-                    <CodeIcon fontSize="large"/>
-                </IconButton>
-                <Box sx={styles.linksArea}>
-                <Link href="#projects"> 
-                <a>
-                <Typography sx={styles.links}>PROJECTS</Typography>
-                </a>
-                    
-                </Link>
-                <Link href="#aboutMe">
-                    <a>
-                    <Typography sx={styles.links}>ABOUT ME</Typography>
-                    </a>
-                   
-                </Link>
-                </Box>
-               
-               
-               
-            </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <Box className={styles.navContainer}>
+        <IconButton className={styles.navItem}>
+          <CodeIcon fontSize="large" sx={{ color: "#34B3F1" }} />
+        </IconButton>
+        <Box className={styles.linksArea}>
+          <Link href="#projects">
+            <a>
+              <Typography className={styles.links}>PROJECTS</Typography>
+            </a>
+          </Link>
+          <Link href="#aboutMe">
+            <a>
+              <Typography className={styles.links}>ABOUT ME</Typography>
+            </a>
+          </Link>
+        </Box>
+      </Box>
+    </>
+  );
+};
