e01c994cdbe48c7454947b9f8587417fe0004a8d
ThalesMaia <thalesmaiaalexandre@gmail.com>
2022-09-28 21:13:17 -0300

    change projectscards

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -1,18 +1,20 @@
-import { Avatar, Grid, Typography } from "@mui/material"
-import { useRouter } from "next/router"
-import { styles } from "./styles"
+import { Grid, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import styles from "./contact.module.scss";
 
 export const ContactMe = () => {
+  const router = useRouter();
 
-    const router = useRouter();
-    
-
-    return (
-        <Grid  container>
-            <Grid item xs={12} md={4} onClick={() => router.push("/contact")}>
-            <Typography sx={styles.contact}>FEEL FREE TO CONTACT ME </Typography>
-            <Typography sx={styles.contact}>{"LET'S GET TO WORK TOGETHER"}</Typography>
-            </Grid>
-        </Grid>
-    )
-}
+  return (
+    <Grid container>
+      <Grid item xs={12} md={4} onClick={() => router.push("/contact")}>
+        <Typography className={styles.contact}>
+          FEEL FREE TO CONTACT ME{" "}
+        </Typography>
+        <Typography className={styles.contact}>
+          {"LET'S GET TO WORK TOGETHER"}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
--- components/email/email.tsx
+++ components/email/email.tsx
@@ -1,44 +1,43 @@
 import { Grid, TextField } from "@mui/material";
 import React from "react";
-import { styles } from "./styles";
+import styles from "./email.module.scss";
 
 interface Props {
-    label: string;
-    value: string;
-    onChange: any;
-    xs?: number;
-    error?: any;
-    helperText?: any;
-    multiline?: boolean;
-    rows?: number;
+  label: string;
+  value: string;
+  onChange: any;
+  xs?: number;
+  error?: any;
+  helperText?: any;
+  multiline?: boolean;
+  rows?: number;
 }
 
-export const EmailField: React.FC<Props> = 
-({
-    label, 
-    value, 
-    onChange, 
-    xs=5, 
-    multiline=false,
-    rows=1,
-    error=false, 
-    helperText=""
+export const EmailField: React.FC<Props> = ({
+  label,
+  value,
+  onChange,
+  xs = 5,
+  multiline = false,
+  rows = 1,
+  error = false,
+  helperText = "",
 }: Props) => {
-
-    return (
-        <Grid item xs={xs} sx={{mb: 3, mr: 3}}>
-            <TextField 
-                    name={label?.toLowerCase()}
-                    label={label}
-                    value={value}
-                    onChange={onChange}
-                    multiline={multiline}
-                    rows={rows}
-                    error={error}
-                    helperText={helperText}
-                    fullWidth
-                    InputProps={{sx: styles.contactInputsBackground}}
-                    InputLabelProps={{sx: styles.contactInputsColor}}/> 
-        </Grid>
-    )
-}
+  return (
+    <Grid item xs={xs}>
+      <TextField
+        name={label?.toLowerCase()}
+        label={label}
+        value={value}
+        onChange={onChange}
+        multiline={multiline}
+        rows={rows}
+        error={error}
+        helperText={helperText}
+        fullWidth
+        InputProps={{ className: styles.contactInputsBackground }}
+        InputLabelProps={{ className: styles.contactInputsColor }}
+      />
+    </Grid>
+  );
+};
